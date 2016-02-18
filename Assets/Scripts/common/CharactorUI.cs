d7e6d6079ba913005bba7094467410c0031cde35
gggg826 <liushida87@126.com>
2016-02-19 01:18:18 +0800

    Create Animations

---
+++ Assets/Scripts/common/CharactorUI.cs
@@ -0,0 +1,34 @@
+﻿/*****************************
+*
+*  Author : TheNO.5
+*
+******************************/
+
+ 
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharactorUI : MonoBehaviour
+{
+    private Image rleg;
+    private Image lleg;
+    private Image body;
+    private Image rhand;
+    private Image lhand;
+    private Image head;
+
+	void Awake()
+    {
+        rleg = transform.Find("RLeg").GetComponent<Image>();
+        lleg = transform.Find("LLeg").GetComponent<Image>();
+        body = transform.Find("Body").GetComponent<Image>();
+        rhand = transform.Find("RHand").GetComponent<Image>();
+        lhand = transform.Find("LHand").GetComponent<Image>();
+        head = transform.Find("Head").GetComponent<Image>();
+    }
+
+    public void InitCharactorUI(string _head)
+    {
+        head.overrideSprite = Resources.Load<Sprite>("Textures/Charactors/" + _head);
+    }
+}
---
+++ Assets/Scripts/common/LoadCharactor.cs
@@ -0,0 +1,25 @@
+﻿/*****************************
+*
+*  Author : TheNO.5
+*
+******************************/
+
+ 
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadCharactor : MonoBehaviour
+{
+    public Transform canvasTran;
+
+    void Start ()
+    {
+        GameObject go = (GameObject)Instantiate(Resources.Load("Prefabs/Charactor"));
+        go.transform.SetParent(canvasTran);
+        go.transform.localPosition = Vector3.zero;
+        CharactorUI charUI = go.GetComponent<CharactorUI>();
+
+        charUI.InitCharactorUI("prisonChar_headg11");
+
+    }
+}
