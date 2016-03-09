6455d41b04538a82c90a5dba6803c41279eead68
gggg826 <liushida87@126.com>
2016-03-09 17:55:07 +0800

    Completed BaseUI,Add the PresenShow on SliderBar

---
+++ Assets/_Scripts/002-BaseGame/AddPlayer.cs
@@ -0,0 +1,17 @@
+﻿/*****************************
+*
+*  Author : TheNO.5
+*
+******************************/
+
+
+using UnityEngine;
+using System.Collections;
+
+public class AddPlayer : MonoBehaviour 
+{
+	void Start () 
+    {
+	    CharactorsController.Instance.AddRoleToScreen("RoleInfo", "小聪明", ROLESTATUS.Naked, transform);
+	}
+}
---
+++ Assets/_Scripts/002-BaseGame/PersentShow.cs
@@ -0,0 +1,20 @@
+﻿/*****************************
+*
+*  Author : TheNO.5
+*
+******************************/
+
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PersentShow : MonoBehaviour 
+{
+    public Slider slider;
+    public Text text;
+    
+	void Update () 
+    {
+	    text.text = slider.value + "/100";
+	}
+}
